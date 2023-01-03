434cc405ebf777f35a4c9bdf41bc37dd47f78c2a
Shee69 <lehoangnamtep@gmail.com>
2023-01-03 17:54:52 +0700

    Add basic files for project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/', (req, res) => {
+    console.log('Here');
+    res.status(500).json({ essage: 'Error' });
+});
+
+module.exports = app;
