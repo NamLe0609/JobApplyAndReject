336e4e233a25e618f99c0390464bfc9306dd801f
Shee69 <lehoangnamtep@gmail.com>
2023-01-12 13:17:04 +0000

    Add new front end page

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'client')));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "client")));
 
-app.get('/', (req, res) => {
-    console.log('Here');
-    res.status(500).json({ essage: 'Error' });
+app.get("/", (req, res) => {
+  console.log("Here");
+  res.status(500).json({ essage: "Error" });
 });
 
 module.exports = app;
